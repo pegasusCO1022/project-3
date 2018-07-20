f46cbed4159c2bc85cb57a92456be6bd41cc9910
pegasusCO1022 <spiderwebb2290@gmail.com>
2018-07-19 22:38:45 -0600

    fixing security issues with keys and working with react

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://Paul:webbad08@ds139951.mlab.com:39951/project-3",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
